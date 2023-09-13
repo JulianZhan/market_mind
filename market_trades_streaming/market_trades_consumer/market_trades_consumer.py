c183ffb992d0565f922ed24742ab574a81439b96
julianzhan <julianzhan6@gmail.com>
2023-09-13 19:41:46 +0800

    connect consumer to kafka topic successfully

--- market_trades_streaming/market_trades_consumer/market_trades_consumer.py
+++ market_trades_streaming/market_trades_consumer/market_trades_consumer.py
@@ -1,17 +1,21 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf, col, explode, current_timestamp, avg
+from pyspark.sql.functions import udf, col, explode, current_timestamp
 from pyspark.sql.avro.functions import from_avro
-from pyspark.sql.streaming import Trigger
+import sys
+
+sys.path.append("../")
 from config import Config
 import os
 
 # setting required packages for pysaprk and kafka integration
+os.environ["JAVA_HOME"] = "/opt/homebrew/opt/java/"
+SPARK_VERSION = "3.4.1"
 os.environ[
     "PYSPARK_SUBMIT_ARGS"
-] = "--packages org.apache.spark:spark-streaming-kafka-0-10_2.12:3.4.1,org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.1"
+] = f"--packages org.apache.spark:spark-streaming-kafka-0-10_2.12:{SPARK_VERSION},org.apache.spark:spark-sql-kafka-0-10_2.12:{SPARK_VERSION},org.apache.spark:spark-avro_2.12:{SPARK_VERSION} pyspark-shell"
 
 
-park = SparkSession.builder.appName("Kafka_cdc").master("local[*]").getOrCreate()
+spark = SparkSession.builder.appName("trades_consumer").master("local[*]").getOrCreate()
 spark.sparkContext.setLogLevel("ERROR")
 # topic name rule: topic_prefix.schema.table
 KAFKA_TOPIC_NAME = f"{Config.KAFKA_TOPIC_NAME}"
@@ -27,6 +31,7 @@ raw_df = (
     .load()
 )
 
+print(raw_df.printSchema())
 
 # Explode the data from Avro
 decoded_df = (
@@ -46,3 +51,8 @@ final_df = (
     .withColumn("trade_timestamp", (col("trade_timestamp") / 1000).cast("timestamp"))
     .withColumn("created_at", current_timestamp())
 )
+
+query1 = raw_df.writeStream.outputMode("append").format("console").start()
+query2 = final_df.writeStream.format("console").start()
+query1.awaitTermination()
+query2.awaitTermination()
