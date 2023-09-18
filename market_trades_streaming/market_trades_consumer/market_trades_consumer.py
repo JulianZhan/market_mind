cceac0be499911ea1e3974170ccd9ace6211826c
julianzhan <julianzhan6@gmail.com>
2023-09-18 19:36:38 +0800

    increase batch size to reduce io stress on db

--- market_trades_streaming/market_trades_consumer/market_trades_consumer.py
+++ market_trades_streaming/market_trades_consumer/market_trades_consumer.py
@@ -89,7 +89,7 @@ if __name__ == "__main__":
     # define kafka and mysql connection details
     kafka_topic_name = f"{Config.KAFKA_TOPIC_NAME}"
     kafka_bootstrap_server = f"{Config.KAFAK_SERVER}:{Config.KAFKA_PORT}"
-    batch_size = "100"
+    batch_size = "1000"
     trades_schema = open("trades_schema.avsc", "r").read()
     jdbc_url = (
         f"jdbc:mysql://{Config.RDS_HOSTNAME}:{Config.RDS_PORT}/{Config.RDS_DB_NAME}"
