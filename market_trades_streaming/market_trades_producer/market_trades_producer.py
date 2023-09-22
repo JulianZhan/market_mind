e4cd566420785d07b7539656134185943d33690e
julianzhan <julianzhan6@gmail.com>
2023-09-23 00:58:45 +0800

    modify task and service definition

--- market_trades_streaming/market_trades_producer/market_trades_producer.py
+++ market_trades_streaming/market_trades_producer/market_trades_producer.py
@@ -126,7 +126,7 @@ if __name__ == "__main__":
     reset_thread.start()
 
     # open websocket connection
-    websocket.enableTrace(False)
+    websocket.enableTrace(True)
     ws = websocket.WebSocketApp(
         f"wss://ws.finnhub.io?token={Config.FINNHUB_API_KEY}",
         on_message=on_message,
