02cb8651213a76f9df055d81480c0df6e3873de9
julianzhan <julianzhan6@gmail.com>
2023-10-02 18:05:14 +0800

    add date picker in frontend

--- market_mind_web/frontend/src/views/home_page.js
+++ market_mind_web/frontend/src/views/home_page.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { Client } from "@stomp/stompjs";
 import {
   fetchAlphaVantageData,
   fetchRedditData,
@@ -10,49 +9,51 @@ import AlphaVantageTrend from "../components/market_sentiment_trend";
 import AlphaVantageStat from "../components/market_sentiment_stat";
 import RedditTrend from "../components/market_emotion_trend";
 import RedditBarChart from "../components/market_emotion_bar";
-import RealtimeTradesTimeSeries from "../components/realtime_trades_timeseries";
 
 const HomePage = () => {
   const [alphavantageData, setAlphaData] = useState([]);
   const [alphavantageStatData, setAlphaStatData] = useState([]);
   const [redditData, setRedditData] = useState([]);
   const [redditBarData, setRedditBarData] = useState([]);
-  const [tradesData, setTradesData] = useState([]);
+  const [selectedEndDate, setSelectedEndDate] = useState(new Date()); // default to today
+  const [timeLength, setTimeLength] = useState(7); // default to 7 days
 
   useEffect(() => {
-    // Replace with your date range
-    const startDate = "2023-01-01";
-    const endDate = "2023-12-31";
+    const startDate = new Date(selectedEndDate);
+    startDate.setDate(startDate.getDate() - timeLength);
 
-    fetchAlphaVantageData(startDate, endDate).then((data) =>
-      setAlphaData(data)
-    );
+    fetchAlphaVantageData(
+      startDate.toISOString().split("T")[0],
+      selectedEndDate.toISOString().split("T")[0]
+    ).then((data) => setAlphaData(data));
     fetchAlphaVantageMostRecent().then((data) => setAlphaStatData(data));
-    fetchRedditData(startDate, endDate).then((data) => setRedditData(data));
+
+    fetchRedditData(
+      startDate.toISOString().split("T")[0],
+      selectedEndDate.toISOString().split("T")[0]
+    ).then((data) => setRedditData(data));
     fetchRedditMostRecent().then((data) => setRedditBarData(data));
-
-    // WebSocket setup
-    const socket = new Client({
-      brokerURL: "http://localhost:8080/websocket-endpoint",
-      debug: (str) => console.log(str),
-      reconnectDelay: 5000,
-      heartbeatIncoming: 4000,
-      heartbeatOutgoing: 4000,
-    });
-
-    socket.onConnect = (frame) => {
-      socket.subscribe("/topic/last-30-minutes", (message) => {
-        setTradesData(JSON.parse(message.body));
-      });
-    };
-
-    return () => {
-      socket.deactivate();
-    };
-  }, []);
+  }, [selectedEndDate, timeLength]);
 
   return (
     <div>
+      <div>
+        {/* Date Picker */}
+        <input
+          type="date"
+          value={selectedEndDate.toISOString().split("T")[0]}
+          onChange={(e) => setSelectedEndDate(new Date(e.target.value))}
+        />
+
+        {/* Time Length Dropdown */}
+        <select
+          value={timeLength}
+          onChange={(e) => setTimeLength(Number(e.target.value))}
+        >
+          <option value={7}>7 days</option>
+          <option value={14}>14 days</option>
+        </select>
+      </div>
       <AlphaVantageTrend
         data={alphavantageData}
         dataKey="avgScore"
@@ -61,7 +62,6 @@ const HomePage = () => {
       <AlphaVantageStat data={alphavantageStatData} />
       <RedditTrend data={redditData} title="Market Emotion" />
       <RedditBarChart data={redditBarData} title="Market Emotion" />
-      <RealtimeTradesTimeSeries data={tradesData} />
     </div>
   );
 };
