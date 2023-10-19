f9f7617718916cef7f4ebec3da012dc27a9a13d5
julianzhan <julianzhan6@gmail.com>
2023-10-19 18:26:06 +0800

    refactor api_utils code to make it cleaner

--- api_server/api_utils.py
+++ api_server/api_utils.py
@@ -21,6 +21,14 @@ engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
 # avro schema for the kafka message
 avro_schema = avro.schema.parse(open("trades_schema.avsc", "r").read())
+# score definition
+score_definition = {
+    "Bearish": "score <= -0.35",
+    "Somewhat-Bearish": "-0.35 < score <= -0.15",
+    "Neutral": "-0.15 < score < 0.15",
+    "Somewhat-Bullish": "0.15 <= score < 0.35",
+    "Bullish": "score >= 0.35",
+}
 
 
 def decode_avro_message(message: dict, schema) -> dict:
@@ -74,13 +82,7 @@ def get_news_sentiment(target_date: str) -> dict:
             .first()
         )
         news_sentiment_dict = {
-            "score_definition": {
-                "Bearish": "score <= -0.35",
-                "Somewhat-Bearish": "-0.35 < score <= -0.15",
-                "Neutral": "-0.15 < score < 0.15",
-                "Somewhat-Bullish": "0.15 <= score < 0.35",
-                "Bullish": "score >= 0.35",
-            },
+            "score_definition": score_definition,
             "score": {
                 "avg_score": news_sentiment.avg_score,
                 "max_score": news_sentiment.max_score,
@@ -90,6 +92,15 @@ def get_news_sentiment(target_date: str) -> dict:
             "date_recorded": target_date,
         }
         return news_sentiment_dict
+
+
+def transform_reddit_emotion(reddit_emotion, target_date: str) -> dict:
+    # transform the reddit emotion from sqlalchemy object to dict
+    reddit_emotion_dict = {"emotion": {}, "date_recorded": target_date}
+    # for each emotion in the target date, add it to the dict
+    for emotion in reddit_emotion:
+        reddit_emotion_dict["emotion"][emotion.emotion_name] = emotion.avg_score
+    return reddit_emotion_dict
 
 
 def get_reddit_emotion(target_date: str) -> dict:
@@ -108,8 +119,5 @@ def get_reddit_emotion(target_date: str) -> dict:
             .filter(func.date(RedditAgg.date_recorded) == target_date)
             .all()
         )
-        reddit_emotion_dict = {"emotion": {}}
-        for emotion in reddit_emotion:
-            reddit_emotion_dict["emotion"][emotion.emotion_name] = emotion.avg_score
-        reddit_emotion_dict["date_recorded"] = target_date
+        reddit_emotion_dict = transform_reddit_emotion(reddit_emotion, target_date)
         return reddit_emotion_dict
