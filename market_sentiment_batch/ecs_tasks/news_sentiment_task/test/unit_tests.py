7ecb53fd74611771615c88548136a7c0a58bf449
julianzhan <julianzhan6@gmail.com>
2023-10-22 14:46:12 +0800

    add comment for test case

--- market_sentiment_batch/ecs_tasks/news_sentiment_task/test/unit_tests.py
+++ market_sentiment_batch/ecs_tasks/news_sentiment_task/test/unit_tests.py
@@ -6,26 +6,39 @@ from news_sentiment_utils import (
 
 
 def test_get_news_sentiment_data_successful_response():
+    """
+    test if get_news_sentiment_data function returns news sentiment data
+    """
     mock_response = Mock()
     mock_response.status_code = 200
     mock_response.json.return_value = {"feed": [{"key": "value"}], "items": 1}
+
+    # use patch to mock requests.get with mock_response
     with patch("requests.get", return_value=mock_response):
         result = get_news_sentiment_data("http://example.com")
         assert result == [{"key": "value"}]
 
 
 def test_get_news_sentiment_data_no_items():
+    """
+    test if get_news_sentiment_data function returns None when no news items found
+    """
     mock_response = Mock()
     mock_response.status_code = 200
     mock_response.json.return_value = {"items": 0}
+    # use patch to mock requests.get with mock_response
     with patch("requests.get", return_value=mock_response):
         result = get_news_sentiment_data("http://example.com")
         assert result is None
 
 
 def test_get_news_sentiment_data_error_response():
+    """
+    test if get_news_sentiment_data function returns None when error response
+    """
     mock_response = Mock()
     mock_response.status_code = 400
+    # use patch to mock requests.get with mock_response
     with patch("requests.get", return_value=mock_response):
         result = get_news_sentiment_data("http://example.com")
         assert result is None
--- market_sentiment_batch/ecs_tasks/reddit_task/test/unit_tests.py
+++ market_sentiment_batch/ecs_tasks/reddit_task/test/unit_tests.py
@@ -4,55 +4,62 @@ from reddit_utils import clean_comment
 
 
 def test_clean_comment():
-    # Define the test case
+    """
+    test if clean_comment function removes special characters and numbers
+    """
     input_text = "@user /u/someone [removed] This is a sample reddit comment http://testurl.com [deleted] with special chars & numbers 12345."
     expected_output = "this is a sample reddit comment with special chars numbers"
 
-    # Assert the result
     assert clean_comment(input_text) == expected_output
 
 
 def test_clean_comment_empty():
-    # Define the test case
+    """
+    test if clean_comment function returns empty string when input is empty
+    """
     input_text = ""
     expected_output = ""
 
-    # Assert the result
     assert clean_comment(input_text) == expected_output
 
 
 def test_clean_comment_none():
-    # Define the test case
+    """
+    test if clean_comment function returns empty string when input is None
+    """
     input_text = None
     expected_output = ""
 
-    # Assert the result
     assert clean_comment(input_text) == expected_output
 
 
 def test_clean_comment_numbers():
-    # Define the test case
+    """
+    test if clean_comment function removes numbers
+    """
     input_text = "12345"
     expected_output = ""
 
-    # Assert the result
     assert clean_comment(input_text) == expected_output
 
 
 def test_clean_comment_special_chars():
-    # Define the test case
+    """
+    test if clean_comment function removes special characters
+    """
     input_text = "!@#$%^&*()_+-=[]{}|;':\",./<>?"
     expected_output = ""
 
-    # Assert the result
     assert clean_comment(input_text) == expected_output
 
 
 def test_clean_comment_normal_text():
-    # Define the test case
+    """
+    test if clean_comment function returns same text when input is normal text
+    """
     input_text = "This is a sample reddit comment without any special chars or numbers."
     expected_output = (
         "this is a sample reddit comment without any special chars or numbers"
     )
-    # Assert the result
+
     assert clean_comment(input_text) == expected_output
